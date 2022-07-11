f3ab56a93b0033657839495df98dc13ab5286ffe
Miguel Trejo <miguel.trejo@upeu.edu.pe>
2022-07-11 07:44:05 -0500

    vs3

--- pyfinal2/src/main/java/pe/org/chaclacayo/pyfinal2/controller/HomeController.java
+++ pyfinal2/src/main/java/pe/org/chaclacayo/pyfinal2/controller/HomeController.java
@@ -1,7 +1,10 @@
 
 package pe.org.chaclacayo.pyfinal2.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -11,9 +14,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     
-    @GetMapping
-    public String index(){
+    @GetMapping("/login")
+    public String login(Model model, @AuthenticationPrincipal User user) {
+        //model.addAttribute("user", user.getUsername());
         return "login";
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("titulo", "Bienvenido a Thymeleaf");
+        return "/main_mp";
     }
     @GetMapping("/register")
     public String register(){
